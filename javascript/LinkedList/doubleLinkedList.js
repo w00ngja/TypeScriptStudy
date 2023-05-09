9f21576c22e60397926945e3a34609140c8a6b5d
Jaewoong Hwang <noa9925@gmail.com>
2023-05-09 20:49:08 +0900

    20230508
    - DLL

---
+++ javascript/LinkedList/doubleLinkedList.js
@@ -0,0 +1,76 @@
+// 1. Skeleton
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DLL {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = ths.head;
+    } else {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    var temp = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      temp.prev = null;
+    }
+    this.length--;
+    return temp;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    var temp = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = temp.next;
+      this.head.prev = null;
+      temp.next = null;
+
+      this.length--;
+      return temp;
+    }
+  }
+
+  unshift(val) {
+    var newNode = new Node(val);
+
+    if (length === 0) {
+      this.tail = newNode;
+      this.head = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
