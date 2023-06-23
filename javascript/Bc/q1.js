0d9b17f8cbadfad8576ad8f94bb2b9d82ac97c54
Jaewoong Hwang <noa9925@gmail.com>
2023-06-23 19:28:31 +0900

    20230623

--- javascript/Bc/q1.js
+++ javascript/Bc/q1.js
@@ -5,10 +5,6 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-function printTower(param0) {
-  hanoiTower(currentStage + 1, 1, 2, 3);
-}
-
 function hanoiTower(n, start, mid, end) {
   if (n === 1) {
     console.log(`Move disk 1 from ${start} to ${end}`);
@@ -19,9 +15,3 @@ function hanoiTower(n, start, mid, end) {
   console.log(`Move disk ${n} from ${start} to ${end}`);
   hanoiTower(n - 1, mid, start, end);
 }
-
-rl.question("진행 단계를 입력하세요: ", (param0) => {
-  const stage = parseInt(param0);
-  printTower(stage);
-  rl.close();
-});
